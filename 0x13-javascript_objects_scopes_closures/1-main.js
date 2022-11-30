9893bb4ebf9269f7cde6ef8fe16d0dffa3ea35f7
kouzin1 <samuelakinwunmi2@gmail.com>
2022-11-30 14:09:19 -0800

    2 Rectangle

--- 0x13-javascript_objects_scopes_closures/1-main.js
+++ 0x13-javascript_objects_scopes_closures/1-main.js
@@ -7,10 +7,11 @@ console.log(r1.width);
 console.log(r1.height);
 
 const r2 = new Rectangle(2, -3);
+console.log(r2);
 console.log(r2.width);
 console.log(r2.height);
 
-comnst r3 = new Rectangle(2);
+const r3 = new Rectangle(2);
 console.log(r3);
 console.log(r3.width);
 console.log(r3.height);
