a019a31ac9a6a9e1259caf3c1d3c8a802931de3c
kouzin1 <samuelakinwunmi2@gmail.com>
2022-12-19 13:35:57 -0800

    Only STatus code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to a URL passed as an argument, and displays only the status code of the response. no pipe
-awk 'NK==1{printf "%s", $2}' test7 $(curl -SI "$1" -o test7)
+awk 'NK==1{printf "%s", $2}' test7 $(curl -sI "$1" -o test7)
